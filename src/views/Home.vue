df276352055e544b9152581c975ec58f4392a3c1
Jim Fowler <fowler@math.osu.edu>
2023-01-04 09:08:06 -0500

    include correct buttons on landing page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,22 @@
 <template>
   <v-container fluid fill-height>
         <v-flex xs12 sm12>
-<h1>Apply to the Ross Mathematics Program</h1>
+<h1>Apply to the 2023 Ross Mathematics Program</h1>
 <p>This website will help you as you navigate the Ross application process.</p>
 <p>If you run into any trouble, please feel free to contact <a href="mailto:ross@rossprogram.org">ross@rossprogram.org</a> for assistance.</p>
 
-<p v-if="!profile">The first step is to <router-link to="/signup">sign up for an account</router-link> by providing your email address; a password will be emailed to you.</p>
+<p v-if="!profile">The first step is to <router-link to="/signup">sign up for an account</router-link> by providing your email address; a password will be emailed to you.  Even if you created an account in 2022, you must create a new account for 2023.</p>
 	    <v-btn v-if="!profile" to="/signup" color="primary">Create a Ross account</v-btn>
 
-<!-- <p v-if="profile">After you have created an account, you can begin to <router-link to="/apply">work on your application</router-link>.</p>
+<p v-if="profile">After you have created an account, you can begin to <router-link to="/apply">work on your application</router-link>.</p>
 
-<p><v-btn v-if="profile" to="/apply" color="primary">Work on your application</v-btn></p> -->
+<p><v-btn v-if="profile" to="/apply" color="primary">Work on your application</v-btn></p>
 
-
+<!--
 <p v-if="offer && offer.offer == 'accept'"><v-btn v-if="profile" to="/status" color="primary">Provide travel information</v-btn></p>
 <p v-else><v-btn v-if="profile" to="/status" color="primary">View status</v-btn></p>
 <p v-if="offer && offer.offer == 'accept'"><v-btn v-if="profile" to="/fees" color="primary">Pay course fees</v-btn></p>
+-->
 
 </v-flex>
 </v-container>
